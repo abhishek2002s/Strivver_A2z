37fa96d41a4b5bb632ca2e59b79482bcbf6af6a8
abhishek2002s <linkedin23356abhi@gmail.com>
2023-11-29 23:54:17 +0530

    StriverA2z

---
+++ BasicMath/q6.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//check prime number
+
+bool isPrime(int N) {
+  for (int i = 2; i < sqrt(N); i++) {
+    if (N % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+int main() {
+
+  int n = 11;
+
+  bool ans = isPrime(n);
+  if (n != 1 && ans == true) {
+    cout << "Prime Number";
+  } else {
+    cout << "Non Prime Number";
+  }
+  return 0;
+}
